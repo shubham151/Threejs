23dd1edec5d5c705bd7cedf6ddc2b41741d2e797
Shubham Mishra <shubham.cmishra@gmail.com>
2024-07-21 01:36:32 -0700

    Working on Sphere

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// console.log(THREE)
+//Scene
+const scene = new THREE.Scene();
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//Sizes
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
 
-setupCounter(document.querySelector('#counter'))
+
+//Create a sphere
+const geometry = new THREE.SphereGeometry(3, 64, 64);
+const material = new THREE.MeshStandardMaterial({
+  color: "#00ff83"
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+//Light
+const light = new THREE.PointLight(0xffffff, 100 );
+light.position.set(10, 0, 10);
+scene.add(light);
+
+//Camera
+const camera = new THREE.PerspectiveCamera(45, size.width/ size.height, 0.1,100);
+camera.position.z = 10;
+scene.add(camera);
+
+
+
+
+//Render
+
+const canvas = document.querySelector(".webgl");
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(size.width, size.height);
+renderer.render(scene, camera)
+
+//Controls
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true
+
+//Resize
+
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth,
+  size.height = window.innerHeight
+
+
+  //Update camera
+
+  
+  camera.aspect = size.width/ size.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(size.width, size.height)
+  renderer.render(scene, camera)
+
+});
+
+const animate = () => {
+  renderer.scene(scene, camera)
+  window.requestAnimationFrame(animate)
+}
+
+animate()
